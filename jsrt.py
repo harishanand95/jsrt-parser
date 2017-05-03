6385ad6c7d1e1159d98eabecce1a0feb890c78a0
Harish Anand <harishanand95@gmail.com>
2017-05-03 17:14:16 +0530

    Feature: Added description in csv file to JsrtImage.  Added age, position, sex parameters to the Image data.
    Signed-off-by: Harish Anand <harishanand95@gmail.com>

--- jsrt.py
+++ jsrt.py
@@ -1,7 +1,7 @@
 import numpy as np
 # from scipy.misc import imresize, imread
 import matplotlib.pyplot as plt
-# import csv
+from csv import reader, excel_tab
 from os import listdir
 
 
@@ -22,6 +22,9 @@ class JsrtImage(object):
         self._x_coordinate = None
         self._y_coordinate = None
         self._diagnosis = None
+        self._age = None
+        self._sex = None
+        self._position = None
         self.load()
 
     def load(self):
@@ -41,10 +44,52 @@ class JsrtImage(object):
             self._image_type = "non-nodule"
         return self._image_type
 
+    @image_type.setter
+    def image_type(self, value):
+        self._image_type = value
+
     def display(self):
         plt.imshow(self.image, cmap=plt.get_cmap('gray'))
         plt.show()
         return self
+
+    # def display_all_data(self):
+    #     print self._degree_of_subtlety
+    #     print self._nodule_size
+    #     print self._age
+    #     print self._sex
+    #     print self._x_coordinate
+    #     print self._y_coordinate
+    #     print self._malignant_or_benign
+    #     print self._position
+    #     print self._diagnosis
+
+    def add_description(self, data, has_nodule=False):
+        """This function adds additional details to the JsrtImage object.
+
+        Args:
+            data       (list): data is a list of the form [filename, degree of subtlety, nodule size [mm], age, sex,
+                               x coordinate of the nodule, y coordinate of the nodule, pathology, position, diagnosis]
+                               for images with nodules and [filename, age, sex, diagnosis("non-nodule")] for non-nodule
+                               images respectively.
+            has_nodule (bool): True for images with nodules and False for images without nodules.
+        """
+        if has_nodule is True:
+            self.image_type = "has nodule"
+            self._degree_of_subtlety = data[1]
+            self._nodule_size = data[2]
+            self._age = data[3]
+            self._sex = data[4]
+            self._x_coordinate = data[5]
+            self._y_coordinate = data[6]
+            self._malignant_or_benign = data[7]
+            self._position = data[8]
+            self._diagnosis = data[9]
+
+        elif has_nodule is False:
+            self._image_type = "non-nodule"
+            self._age = data[1]
+            self._sex = data[2]
 
 
 class Jsrt(object):
@@ -55,6 +100,7 @@ class Jsrt(object):
         self._has_nodule_image_list = None
         self._non_nodule_image_list = None
         self.__get_images_list()
+        self.add_descriptions_to_image()
 
     def __get_images_list(self):
         images_list = listdir(self._images_dir)
@@ -73,22 +119,91 @@ class Jsrt(object):
 
     @staticmethod
     def _load_images_from_file(filenames, directory):
-        """ _load_images_from_file loads images located at folder + filename into a list of JsrtImage objects.
-            Input  : filenames - a list of image file names
-                     directory - path to the directory where all images are present
-            Output : [JsrtImage objects]
+        """This function load images (not image) located at directory/filename and creates an image object from it.
+        Args:
+            filenames (list): a list of names of the image files.
+            directory  (str): path to the directory/folder where all images are present
+        Returns :
+            a list of JsrtImage objects
         """
         images_list = []
-        for image_path in filenames:
-            img = JsrtImage(directory + image_path)
+        for image_name in filenames:
+            img = JsrtImage(directory + image_name)
             images_list.append(img)
         return images_list
 
+    def clean_csv_file(self, file_path, file_type):
+        """This function cleans the csv data present along with the image file. The data is removed of inappropriate
+        splits and combined into a 10 element list for images with nodules and 4 element list for images without
+        nodules.
+        csv content is formatted into [filename, degree of subtlety, nodule size [mm], age, sex, x coordinate
+        of the nodule, y coordinate of the nodule, pathology, position, diagnosis] format for images with nodules
+        and [filename, age, sex, diagnosis("non-nodule")] format for non-nodule images respectively.
+
+        Args:
+            file_path (str) : Path to the CSV file for nodule and non-nodule images.
+            file_type (str) : If file_path is to nodule images, then file_type is "nodule csv" and if file_path is to
+                              non-nodule images, then file_type is "non nodule csv".
+
+        Returns:
+            csv_data (dict): csv_data returns a dict of the form { 'filename' : 'filename' image's csv data }.
+
+            Example:
+                { "./All247images/JPCNN010.IMG" : ['JPCNN010.IMG', '54', 'male', 'non-nodule'] }
+
+        """
+        csv_data = {}
+        if file_type == "nodule csv":
+            with open(file_path, "rU") as csv_file:
+                content = reader(csv_file, dialect=excel_tab)
+                for row in content:
+                    if len(row) != 0:
+                        row[8:10] = [" ".join(row[8:10])]
+                        row[9:] = [" ".join(row[9:])]
+                        csv_data[self._images_dir + row[0]] = row
+            return csv_data
+        elif file_type == "non nodule csv":
+            with open(file_path, "rU") as csv_file:
+                content = reader(csv_file, dialect=excel_tab)
+                for row in content:
+                    if len(row) != 0:
+                        row = row[0].split(" ")
+                        new_row = []
+                        new_row.extend([row[0], row[1], row[3], row[len(row) - 1]])
+                        csv_data[self._images_dir + new_row[0]] = new_row
+            return csv_data
+        else:
+            return csv_data
+
+    def add_descriptions_to_image(self):
+        """ This function fetches the descriptions of the images present in the csv file and adds them
+            to the JsrtImage object.
+        """
+        csv_data = self.clean_csv_file("Clinical_Information/CLNDAT_EN.txt", "nodule csv")
+
+        for image in self._has_nodule_image_list:
+            print image.image_path
+            print csv_data[image.image_path]
+            image.add_description(csv_data[image.image_path], has_nodule=True)
+
+        csv_data = self.clean_csv_file("Clinical_Information/CNNDAT_EN.TXT", "non nodule csv")
+
+        for image in self._non_nodule_image_list:
+            print image.image_path
+            print csv_data[image.image_path]
+            image.add_description(csv_data[image.image_path], has_nodule=False)
+
     def get_images(self, has_nodule=True, num_of_images=1):
-        """ get_images function returns "num_of_images" number of JsrtImage objects in a list.
-        Function takes a bool has_nodule which return images with nodules when True and non-nodule images when False.
-        Input  : has_nodule (defaults to True), num_of_images (defaults to 1).
-        Output : [JsrtImage object]
+        """This function gives "num_of_images" number of JsrtImage objects in a list. The objects can be either all
+        image with nodules or non-nodules.
+
+        Args:
+            has_nodule   (bool): Defaults to True. Images with the nodules are selected when it is set to True and
+                                images without nodules are selected when it is set to False.
+            num_of_images (int): Defaults to 1. The required number of images is given.
+
+        Returns :
+            a list of JsrtImage objects. Total objects will be num_of_images.
         """
         if has_nodule is True:
             if len(self._has_nodule_image_list) < num_of_images:
@@ -103,5 +218,6 @@ class Jsrt(object):
 
 
 dataset = Jsrt("./All247images/")
-image = dataset.get_images()
-image[0].display()
+pic = dataset.get_images()
+# pic[0].display_all_data()
+
